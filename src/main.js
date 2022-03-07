19885c8c2b363edcfd8ac8e4b064434d49e7be18
Luisa006 <lufermartinez0618@gmail.com>
2022-03-07 15:57:43 -0500

    HTML y CSS pag 1 y 2 maquetación

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+/*import { myFunction } from './lib/index.js';
 
-myFunction();
+myFunction();*/
+document.querySelector("#join").addEventListener("click", changeP1)
+function changeP1() {
+    document.getElementById("pag1").style.display= "none";
+    document.getElementById("pag2").style.display= "block";
+}
