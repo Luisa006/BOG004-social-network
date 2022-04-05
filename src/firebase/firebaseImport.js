42bebe2bbf9c8ada58109691886838a2c0c7540d
unknown <michell16v@gmail.com>
2022-04-05 09:20:43 -0500

    CRUD, funcion de publicar, borrar y parte de editar post

--- src/firebase/firebaseImport.js
+++ src/firebase/firebaseImport.js
@@ -1,7 +1,7 @@
 import { getAuth, createUserWithEmailAndPassword, sendEmailVerification, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js";
 import { GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js";
-import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-firestore.js";
+import { getFirestore, collection, addDoc, getDocs, deleteDoc, doc, setDoc } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-firestore.js";
 export { getAuth, createUserWithEmailAndPassword, sendEmailVerification,
   signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup,
-  getFirestore, collection, addDoc, getDocs,
+  getFirestore, collection, addDoc, getDocs, deleteDoc, doc, setDoc,
 };
